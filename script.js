cf718f5ef72fc423142ec68f238c3c1a78de7d65
firasRoggai <firasreggai@gmail.com>
2022-07-13 14:22:58 +0100

    added comments

--- script.js
+++ script.js
@@ -2,6 +2,11 @@
 const titles = Object.values(document.querySelector('article').children);
 const sideBare = document.querySelector('.sidebar');
 /*** End Variables ***/
+/*** Script:
+ * It takes all the element with the class title or sub , and makes
+ *  the sidebar accordingly 
+ ***/
+
 titles.forEach((element)=>{
     if(element.className.includes("title")){
     const newElement = document.createElement('h1')
